a813ded112eff42675d96a1115ee5c4239e9d742
Joao-Moura <jaummoura15@gmail.com>
2022-04-20 19:28:09 -0300

    Adicionando implementação de salas
    Co-authored-by: brenno-silva <brenno.silva037@gmail.com>

--- server.c
+++ server.c
@@ -4,42 +4,99 @@
 #include <string.h>
 #include <arpa/inet.h>
 
-
 #define STDIN 0
+#define MAX_SALAS 100
 
 
-fd_set master, read_fds, write_fds;
+fd_set master, read_fds;
 struct sockaddr_in myaddr, remoteaddr;
-int fdmax, sd, newfd, i, j, nbytes, yes=1, addrlen;
+int fdmax, newfd, nbytes, yes=1, addrlen;
 char buf[256];
+
+typedef struct {
+    int cliente_sd;
+    char nome[256];
+    int ativo;
+} cliente;
+
+typedef struct {
+    fd_set sala_fd;
+    int limite;
+    int quantidade;
+    int ativo;
+    cliente *clientes;
+} sala;
+
+sala salas[MAX_SALAS];
 
 
 void
-envia_msg () {
-    printf("Enviando mensagem do file descriptor %d\n", i);
+envia_msg (int sd, int sala_id, int server_sd) {
+    printf("Enviando mensagem do file descriptor %d na sala %d\n", sd, sala_id);
     // Para cada file descriptor
-    for (j = 0; j <= fdmax; j++)
+    for (int j = 0; j <= fdmax; j++)
         // checa se ele esta no cesto do master
-        if (FD_ISSET(j, &master))
+        if (FD_ISSET(j, &salas[sala_id].sala_fd))
             // e checa se o valor nao e o descritor de si mesmo
-            if (j != i && j != sd)
+            if (j != sd && j != server_sd)
                 // por fim envia a mensagem para aquele socket descritor
                 send(j, buf, nbytes, 0);
 }
 
 
 void
-executa_comando () {
+executa_comando (int sd) {
     char resp_buf[256];
 
     // Se o recv retornar 0 ou a mensagem foi de sair
     // retira o socket descriptor do cesto
     if (strncmp(buf+1, "sair", 4) == 0) {
-        printf("Desconectando descritor %d\n", i);
+        printf("Desconectando descritor %d\n", sd);
         strcpy(resp_buf, "Cliente Desconectado\n");
-        send(i, resp_buf, strlen(resp_buf), 0);
-        close(i);
-        FD_CLR(i, &master);
+        send(sd, resp_buf, strlen(resp_buf), 0);
+        close(sd);
+        FD_CLR(sd, &master);
+    }
+}
+
+
+void
+inicia_servidor () {
+    for (int i = 0; i < MAX_SALAS; i++) {
+        FD_ZERO(&salas[i].sala_fd);
+        salas[i].limite = 0;
+        salas[i].ativo = 0;
+    }
+}
+
+
+int
+cria_sala (int limite) {
+    int sala;
+    for (sala = 0; sala < MAX_SALAS; sala++)
+        if (salas[sala].ativo == 0)
+            break;
+
+    salas[sala].ativo = 1;
+    salas[sala].limite = limite;
+    salas[sala].clientes = malloc(limite * sizeof(cliente));
+    for (int i = 0; i < limite; i++)
+        salas[sala].clientes[i].ativo = 0;
+
+    return sala;
+}
+
+
+void
+inserir_na_sala(int sd, int sala, char nome[], int tam_nome) {
+    salas[sala].quantidade++;
+    FD_SET(sd, &salas[sala].sala_fd);
+    for (int i = 0; i < salas[sala].limite; i++) {
+        if (salas[sala].clientes[i].ativo == 0) {
+            salas[sala].clientes[i].ativo = 1;
+            salas[sala].clientes[i].cliente_sd = sd;
+            strncpy(salas[sala].clientes[i].nome, nome, tam_nome);
+        }
     }
 }
 
@@ -52,12 +109,13 @@ main (int argc, char *argv[]) {
         exit(1);
     }
 
-    // Limpeza dos sets master e read_fds
+    // Limpeza dos sets master e das salas
     FD_ZERO(&master);
     FD_ZERO(&read_fds);
+    inicia_servidor();
 
     // Configuracao de socket
-    sd = socket(AF_INET, SOCK_STREAM, 0);
+    int sd = socket(AF_INET, SOCK_STREAM, 0);
     setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
     myaddr.sin_family = AF_INET;
@@ -78,12 +136,14 @@ main (int argc, char *argv[]) {
     // addrlen -> tamnaho da struct addrlen
     addrlen = sizeof(remoteaddr);
 
+    int sala;
+
     for ( ; ; ) {
         // Informa que o master ira receber descritores de leitura e realiza o select
         read_fds = master;
         select(fdmax+1, &read_fds, NULL, NULL, NULL);
 
-        for (i = 0; i <= fdmax; i++) {
+        for (int i = 0; i <= fdmax; i++) {
             // Testa para ver se o file descriptor esta no cesto
             if (FD_ISSET(i, &read_fds)) {
                 // Checa se o file descriptor e o socket
@@ -91,6 +151,20 @@ main (int argc, char *argv[]) {
                     // Se for, aceita a conexao e adiciona o socket descriptor no cesto
                     newfd = accept(sd, (struct sockaddr *)&remoteaddr, &addrlen);
                     FD_SET(newfd, &master);
+
+                    int limite, tam_nome;
+                    char nome[256];
+                    // send(i, salas, sizeof(sala) * MAX_SALAS, 0);
+                    tam_nome = recv(newfd, nome, sizeof(char) * 256, 0);
+                    recv(newfd, &sala, sizeof(int), 0);
+                    printf("%d\n", sala);
+                    
+                    if (sala == -1) {
+                        recv(newfd, &limite, sizeof(int), 0);
+                        sala = cria_sala(limite);
+                    }
+                    inserir_na_sala(newfd, sala, nome, tam_nome);
+
                     // Se o valor do sd for maior que o atual (mais coisas no cesto)
                     // atualiza esse valor para as proximas iteracoes do loop
                     if (newfd > fdmax)
@@ -99,6 +173,7 @@ main (int argc, char *argv[]) {
                 else {
                     // Se nao for o descritor do socket, cria um buffer, recebe a mensagem
                     // e a retransmite por todos os sockets conectados
+                    recv(i, &sala, sizeof(int), 0);
                     memset(&buf, 0, sizeof(buf));
                     nbytes = recv(i, buf, sizeof(buf), 0);
 
@@ -109,9 +184,9 @@ main (int argc, char *argv[]) {
                     }
 
                     if (buf[0] == '/')
-                        executa_comando();
+                        executa_comando(i);
                     else
-                        envia_msg();
+                        envia_msg(i, sala, sd);
                 }
             }
         }
